c44a8055ea564accaf18f681fb04b9e990cf98fc
Jjolgit <92000949+chewyricecake@users.noreply.github.com>
2023-04-11 15:07:58 -0400

    Gradle Project
    Changed the intellij project into gradle

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static final Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         Generator generator = new Generator(keyboard);
 
         menu();
